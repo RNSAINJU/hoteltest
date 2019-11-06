f3b9fa7833c2708ed134085bf49fa83acaa0a8e3
Rnsainju <aryan.sainju@gmail.com>
2019-11-06 01:20:12 -0500

    responsive

--- services/urls.py
+++ services/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 	path('', TemplateView.as_view(template_name='landing.html'), name='landing'),
 	path('home/', views.HomeView.as_view(), name='home'),
 	path('home/v2', views.HomeView2.as_view(), name='home2'),
-	path('about',views.AboutView.as_view(),name='about'),
+	path('about', TemplateView.as_view(template_name='aboutus.html'), name='about'),
 	path('home/', views.HomeView.as_view(), name='services'),
 	path('home/', views.HomeView.as_view(), name='contactus'),
 ]
