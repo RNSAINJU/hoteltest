a7ed3fd479b8e843117bd04259ea2e4d462822fe
Rnsainju <aryan.sainju@gmail.com>
2019-11-07 05:24:49 -0500

    static file settings fix

--- services/models.py
+++ services/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 
+# import os
+# import random
+# def get_filename_ext(filepath):
+# 	base_name=os.path.basename(filepath)
+# 	name,ext=os.path.splitext(base_name)
+# 	return name,ext
+
+# def upload_image_path(instance, filename):
+# 	new_filename=random.randint(1,3910209312)
+# 	name,ext=get_filename_ext(filename)
+# 	final_filename='{new_filename}{ext}'.format(new_filename=new_filename,ext=ext)
+# 	return "post-image/{final_filename}".format(
+# 		new_filename=new_filename,
+# 		final_filename=final_filename
+# 		)
+
 
 class Package(models.Model):
 	name = models.CharField(max_length=255)
@@ -7,7 +23,6 @@ class Package(models.Model):
 	discounted_price = models.DecimalField(max_digits=12, decimal_places=2)
 	description = models.TextField(blank=True)
 	image = models.ImageField(upload_to='packages/%y/%m/', blank=True, null=True)
-
 	is_active = models.BooleanField(default=True)
 	is_featured = models.BooleanField(default=False)
 	date_created = models.DateTimeField(auto_now_add=True)
